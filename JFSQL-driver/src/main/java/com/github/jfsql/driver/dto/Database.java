f1a64b3097684048d944bf275b3dfd9acff260ec
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-03-12 12:27:18 +0100

    Replacing constructor with annotation in Database

--- JFSQL-driver/src/main/java/com/github/jfsql/driver/dto/Database.java
+++ JFSQL-driver/src/main/java/com/github/jfsql/driver/dto/Database.java
@@ -3,14 +3,13 @@ package com.github.jfsql.driver.dto;
 import java.nio.file.Path;
 import java.util.List;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class Database {
 
     private final Path url;
     private List<Table> tables;
 
-    public Database(final Path url) {
-        this.url = url;
-    }
 }
