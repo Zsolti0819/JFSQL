9bd2814e3f69d92518d3261ff0e52da86aea43ae
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-03-25 01:30:44 +0100

    Minor changes in PropertiesReader

--- JFSQL-driver/src/main/java/com/github/jfsql/driver/util/PropertiesReader.java
+++ JFSQL-driver/src/main/java/com/github/jfsql/driver/util/PropertiesReader.java
@@ -16,18 +16,63 @@ public class PropertiesReader {
 
     public PropertiesReader(final Properties properties) {
         if (properties == null) {
-            logger.info("properties was null, will use the default values.");
+            logger.info("Properties was null, default config will be used: "
+                    + "persistence = {}, "
+                    + "statement.caching = {}, "
+                    + "transaction.versioning = {}, "
+                    + "schema.validation = {}", persistence, statementCaching, transactionVersioning,
+                schemaValidation);
             return;
         }
 
-        readStatementCachingProperty(properties);
         readPersistenceProperty(properties);
+        readStatementCachingProperty(properties);
         readTransactionVersioningProperty(properties);
         readSchemaValidationProperty(properties);
+
+        logger.info("The following config will be used: "
+                + "persistence = {}, "
+                + "statement.caching = {}, "
+                + "transaction.versioning = {}, "
+                + "schema.validation = {}", persistence, statementCaching, transactionVersioning,
+            schemaValidation);
     }
 
-    public boolean isSchemaValidation() {
-        return schemaValidation;
+    private void readPersistenceProperty(final Properties properties) {
+        String persistenceString = properties.getProperty("persistence");
+        if (persistenceString != null) {
+            persistenceString = persistenceString.trim();
+            if (persistenceString.equalsIgnoreCase("xml") || persistenceString.equalsIgnoreCase("json")) {
+                persistence = persistenceString.toLowerCase();
+                logger.trace("Successfully parsed the value '{}' for the key 'persistence', will use '{}'",
+                    persistenceString, persistence);
+            } else {
+                logger.trace(
+                    "Failed to parse the value '{}' for the key 'persistence', will use the default value '{}'",
+                    persistenceString, persistence);
+            }
+        } else {
+            logger.trace("No value was found for key 'persistence', the default value '{}' will be used",
+                persistence);
+        }
+    }
+
+    private void readTransactionVersioningProperty(final Properties properties) {
+        final String transactionVersioningString = properties.getProperty("transaction.versioning");
+        if (transactionVersioningString != null) {
+            try {
+                transactionVersioning = Boolean.parseBoolean(transactionVersioningString);
+                logger.trace("Successfully parsed the value '{}' for the key 'transaction.versioning', will use '{}'",
+                    transactionVersioningString, transactionVersioning);
+            } catch (final IllegalArgumentException e) {
+                logger.trace(
+                    "Failed to parse the value '{}' for the key 'transaction.versioning', the default value '{}' will be used",
+                    transactionVersioningString, transactionVersioning);
+            }
+        } else {
+            logger.trace("No value was found for key 'transaction.versioning', the default value '{}' will be used",
+                transactionVersioning);
+        }
     }
 
     private void readStatementCachingProperty(final Properties properties) {
@@ -35,15 +80,15 @@ public class PropertiesReader {
         if (statementCachingString != null) {
             try {
                 statementCaching = Boolean.parseBoolean(statementCachingString);
-                logger.info("Successfully parsed the value '{}' for the key for 'statement.caching', will use '{}'",
+                logger.trace("Successfully parsed the value '{}' for the key for 'statement.caching', will use '{}'",
                     statementCachingString, statementCaching);
             } catch (final IllegalArgumentException e) {
-                logger.info(
+                logger.trace(
                     "Failed to parse the value '{}' for the key for 'statement.caching', the default value '{}' will be used",
                     statementCachingString, statementCaching);
             }
         } else {
-            logger.info("No value was found for key 'statement.caching', the default value '{}' will be used",
+            logger.trace("No value was found for key 'statement.caching', the default value '{}' will be used",
                 statementCaching);
         }
     }
@@ -53,53 +98,16 @@ public class PropertiesReader {
         if (schemaValidationString != null) {
             try {
                 schemaValidation = Boolean.parseBoolean(schemaValidationString);
-                logger.info("Successfully parsed the value '{}' for the key 'schema.validation', will use '{}'",
+                logger.trace("Successfully parsed the value '{}' for the key 'schema.validation', will use '{}'",
                     schemaValidationString, schemaValidation);
             } catch (final IllegalArgumentException e) {
-                logger.info(
+                logger.trace(
                     "Failed to parse the value '{}' for the key 'schema.validation', the default value '{}' will be used",
                     schemaValidationString, schemaValidation);
             }
         } else {
-            logger.info("No value was found for key 'schema.validation', the default value '{}' will be used",
+            logger.trace("No value was found for key 'schema.validation', the default value '{}' will be used",
                 schemaValidation);
-        }
-    }
-
-    private void readTransactionVersioningProperty(final Properties properties) {
-        final String transactionVersioningString = properties.getProperty("transaction.versioning");
-        if (transactionVersioningString != null) {
-            try {
-                transactionVersioning = Boolean.parseBoolean(transactionVersioningString);
-                logger.info("Successfully parsed the value '{}' for the key 'transaction.versioning', will use '{}'",
-                    transactionVersioningString, transactionVersioning);
-            } catch (final IllegalArgumentException e) {
-                logger.info(
-                    "Failed to parse the value '{}' for the key 'transaction.versioning', the default value '{}' will be used",
-                    transactionVersioningString, transactionVersioning);
-            }
-        } else {
-            logger.info("No value was found for key 'transaction.versioning', the default value '{}' will be used",
-                transactionVersioning);
-        }
-    }
-
-    private void readPersistenceProperty(final Properties properties) {
-        String persistenceString = properties.getProperty("persistence");
-        if (persistenceString != null) {
-            persistenceString = persistenceString.trim();
-            if (persistenceString.equalsIgnoreCase("xml") || persistenceString.equalsIgnoreCase("json")) {
-                persistence = persistenceString.toLowerCase();
-                logger.info("Successfully parsed the value '{}' for the key 'persistence', will use '{}'",
-                    persistenceString, persistence);
-            } else {
-                logger.info(
-                    "Failed to parse the value '{}' for the key 'persistence', will use the default value '{}'",
-                    persistenceString, persistence);
-            }
-        } else {
-            logger.info("No value was found for key 'persistence', the default value '{}' will be used",
-                persistence);
         }
     }
 
