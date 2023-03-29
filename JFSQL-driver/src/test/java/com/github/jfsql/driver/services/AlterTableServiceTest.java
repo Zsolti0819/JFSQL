47b1abfad686566db2ac9dfeb8236d1335c9c2a1
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-03-29 11:56:07 +0200

    Test fix

--- JFSQL-driver/src/test/java/com/github/jfsql/driver/services/AlterTableServiceTest.java
+++ JFSQL-driver/src/test/java/com/github/jfsql/driver/services/AlterTableServiceTest.java
@@ -2,6 +2,8 @@ package com.github.jfsql.driver.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
@@ -19,7 +21,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.SQLException;
 import java.util.Set;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -63,21 +64,20 @@ class AlterTableServiceTest {
         assertEquals("Table name cannot be the same as database name.", thrown.getMessage());
     }
 
-    @Disabled
     @Test
     void testRenameTable_whenFailedToRenameFilesThenRethrowException() throws IOException {
+        when(statement.getNewTableName()).thenReturn("myTableEdited");
         when(reader.getFileExtension()).thenReturn("xml");
         when(reader.getSchemaFileExtension()).thenReturn("xsd");
-        when(databaseManager.getDatabase()).thenReturn(database);
         when(database.getUrl()).thenReturn(Path.of("someUrl"));
         when(table.getTableFile()).thenReturn("tableFile");
         when(table.getSchema()).thenReturn(schema);
         when(schema.getSchemaFile()).thenReturn("schemaFile");
         when(transactionManager.getUncommittedTables()).thenReturn(uncommittedTables);
         when(uncommittedTables.contains(table)).thenReturn(false);
-        doThrow(IOException.class).when(ioOperationHandler).renameFile(table.getTableFile(), "");
+        doThrow(IOException.class).when(ioOperationHandler).renameFile(anyString(), anyString());
         final SQLException thrown = assertThrows(SQLException.class,
             () -> alterTableService.renameTable(statement, database, table));
-        assertEquals("Failed to rename files.\n", thrown.getMessage());
+        assertTrue(thrown.getMessage().contains("Failed to rename files.\n"));
     }
 }
