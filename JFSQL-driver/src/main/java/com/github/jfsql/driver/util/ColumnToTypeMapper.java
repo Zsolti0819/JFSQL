6d19bab54bc40aed7c9ac7a0e2faca86e2dc1372
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-03-31 10:29:38 +0200

    Simplifying ColumnToTypeMapper

--- JFSQL-driver/src/main/java/com/github/jfsql/driver/util/ColumnToTypeMapper.java
+++ JFSQL-driver/src/main/java/com/github/jfsql/driver/util/ColumnToTypeMapper.java
@@ -6,45 +6,20 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class ColumnToTypeMapper {
 
-    /**
-     * @param statement The statement from which the columns will be extracted
-     * @param table     The table in which all the columns are paired to a type
-     * @return Ordered map in which the column names are paired to types. This map can but don't have to contain less
-     * key-value pairs as the table's original columnsAndTypes map.
-     */
     public Map<String, String> mapColumnsToTypes(final StatementWithColumns statement, final Table table) {
         final List<String> statementColumnNames = new ArrayList<>(statement.getColumns());
         final Map<String, String> columnsAndTypes = new LinkedHashMap<>(table.getSchema().getColumnsAndTypes());
-        final List<String> excludedColumnNames = getExcludedColumnNames(statementColumnNames,
-            new ArrayList<>(columnsAndTypes.keySet()));
-
-        // Remove from the map those pairs, which we don't want to select
-        for (final String columnName : excludedColumnNames) {
-            columnsAndTypes.remove(columnName);
-        }
-
         final Map<String, String> orderedColumnsAndTypes = new LinkedHashMap<>();
-        final List<String> columns = new ArrayList<>(columnsAndTypes.keySet());
-        final List<String> types = new ArrayList<>(columnsAndTypes.values());
 
-        for (int i = 0; i < columnsAndTypes.size(); i++) {
-            for (int j = 0; j < columns.size(); j++) {
-                if (Objects.equals(statementColumnNames.get(i), columns.get(j))) {
-                    orderedColumnsAndTypes.put(columns.get(j), types.get(j));
-                }
+        for (final String column : statementColumnNames) {
+            if (columnsAndTypes.containsKey(column)) {
+                orderedColumnsAndTypes.put(column, columnsAndTypes.get(column));
             }
         }
-        return orderedColumnsAndTypes;
-    }
 
-    private List<String> getExcludedColumnNames(final List<String> selectedColumnNames,
-        final List<String> tableColumns) {
-        final List<String> excludedColumnNames = new ArrayList<>(tableColumns);
-        excludedColumnNames.removeAll(selectedColumnNames);
-        return excludedColumnNames;
+        return orderedColumnsAndTypes;
     }
 }
