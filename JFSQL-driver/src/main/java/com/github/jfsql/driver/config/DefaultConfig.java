c38809a340dfe84713302e9e7f7b609d466c6b6b
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-04-30 10:58:40 +0200

    Changing the default config of the driver

--- JFSQL-driver/src/main/java/com/github/jfsql/driver/config/DefaultConfig.java
+++ JFSQL-driver/src/main/java/com/github/jfsql/driver/config/DefaultConfig.java
@@ -3,7 +3,7 @@ package com.github.jfsql.driver.config;
 public class DefaultConfig {
 
     static Persistence persistence = Persistence.JSON;
-    static TransactionVersioning transactionVersioning = TransactionVersioning.JGIT;
+    static TransactionVersioning transactionVersioning = TransactionVersioning.DEFAULT;
     static boolean schemaValidation = true;
     static boolean statementCaching = true;
 
